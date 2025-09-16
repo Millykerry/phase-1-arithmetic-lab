7cdfca8d60d4da98b894e8ca7b35464f0e2b5f98
millykerry <kerryom975@gmail.com>
2025-09-17 02:09:54 +0300

    Add variables num3, num4, mod, and max to pass arithmetic lab tests

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//multiply two numbers so the result is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+//generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1; 
+
+
+//calculate remainder; and should be 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+//find the highest number in a set
+const max = Math.max(1,5,10,15,20);
